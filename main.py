1909bc8a4174901eecec7408c2882dd5329f6649
Rugedi <rugedi@proton.me>
2025-09-04 18:39:07 +0300

    add deltatime

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ def main():
     
     black = (0, 0, 0)
 
+    #delta time
+    clock = pygame.time.Clock()
+    dt  = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
         pygame.display.flip()
-        
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 
     print("Starting Asteroids!")
